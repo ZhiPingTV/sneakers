16bae3d376e4e8abbad456bf8ad25988445af1f2
wangli <wangli@zhiping.tv>
2014-06-23 11:41:21 +0800

    修改代码版本不一致导致的bug

--- lib/sneakers/publisher_ex.rb
+++ lib/sneakers/publisher_ex.rb
@@ -1,34 +1,29 @@
 module Sneakers
-  class PublisherEx
-    attr_accessor :exchange
-
-    def initialize(opts={})
-        @mutex = Mutex.new
-        @opts = Sneakers::Config.merge(opts)
-    end
-
-    def publish(msg, routing)
-        @mutex.synchronize do
-          ensure_connection!() unless connected?
+    class PublisherEx
+        def initialize(opts={})
+            @mutex = Mutex.new
+            @opts = Sneakers::Config.merge(opts)
         end
-        queue_name = Support::QueueName.new(routing[:to_queue], @opts).to_s
-        queue  = @channel.queue(queue_name, :durable => @opts[:durable])
-        Sneakers.logger.info("publishing <#{msg}> to [#{queue_name}]")
-        queue.publish(msg, :routing_key => queue_name)
-    end
 
-  private
+        def publish(msg, routing)
+            @mutex.synchronize do
+                ensure_connection! unless connected?
+            end
+            queue  = @channel.queue(routing[:to_queue], :durable => @opts[:durable])
+            Sneakers.logger.info("publishing <#{msg}> to [#{routing[:to_queue]}]")
+            queue.publish(msg, :routing_key => routing[:to_queue])
+        end
 
-    def ensure_connection!()
-        @bunny = Bunny.new(@opts[:amqp], :vhost => @opts[:vhost], :heartbeat => @opts[:heartbeat])
-        @bunny.start
-        @channel = @bunny.create_channel
-        @channel.prefetch(@opts[:prefetch])
-    end
+        private
 
-    def connected?
-       @bunny && @bunny.connected?
+        def ensure_connection!
+            @bunny = Bunny.new(@opts[:amqp], heartbeat: @opts[:heartbeat], vhost: @opts[:vhost])
+            @bunny.start
+            @channel = @bunny.create_channel
+        end
+
+        def connected?
+           @bunny && @bunny.connected?
+        end
     end
-  end
 end
-
